8e710b6a0c248e817fa24e3c8f6fe820677e66a4
Logy0704 <example@example.com>
2019-03-01 00:29:46 +0900

    radius

--- main.js
+++ main.js
@@ -2,13 +2,6 @@ var state = { count: 0 };
 var app = new Vue({
   el: '#app',
   data: {
-    classObject: {
-      isChild: true,
-      isActive: true
-    },
-    styleObject: {
-      color: 'red',
-      backgroundColor: 'lightgray'
-    }
+    radius: 50
   }
 });
