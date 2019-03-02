06d0ea3ced5d4d65d81697c1d95fc8ebf01c4274
Logy0704 <example@example.com>
2019-03-03 00:30:46 +0900

    list.push

--- main.js
+++ main.js
@@ -5,7 +5,20 @@ var app = new Vue({
     list: [
       { id: 1, name: 'aa', hp: 100 },
       { id: 2, name: 'ab', hp: 200 },
-      { id: 3, name: 'bb', hp: 300 }
+      { id: 3, name: 'bb', hp: 301 }
     ]
+  },
+  methods: {
+    doAdd: function() {
+      var max = this.list.reduce(function(a, b) {
+        return a > b.id ? a : b.id;
+      }, 0);
+
+      this.list.push({
+        id: max + 1,
+        name: this.name,
+        hp: 500
+      });
+    }
   }
 });
