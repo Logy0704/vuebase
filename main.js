221ec9362957cf0bafe90b168aaee207eda1fe20
Logy0704 <example@example.com>
2019-03-03 00:15:14 +0900

    list

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ var state = { count: 0 };
 var app = new Vue({
   el: '#app',
   data: {
-    ok: true
+    list: [
+      { id: 1, name: 'aa', hp: 100 },
+      { id: 2, name: 'ab', hp: 200 },
+      { id: 3, name: 'bb', hp: 300 }
+    ]
   }
 });
