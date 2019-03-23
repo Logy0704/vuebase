a0c8c543db67591b8ae448f98f4c470ffdf9883a
Logy0704 <example@example.com>
2019-03-23 20:45:32 +0900

    computed

--- main.js
+++ main.js
@@ -5,12 +5,17 @@ Vue.filter('readMore', function (text, length, suffix) {
 var app = new Vue({
   el: '#app',
   data: {
-    text: '    Lorem ipsu variation of the ordinary lorem ipsum text has been used in typesetting since the 1960s or earlier, when it was popularized by advertisements for Letraset transfer sheets.It was introduced to the Information Age in the mid - 1980s by Aldus Corporation, which employed it in graphics and word - processing templates for its desktop publishing program PageMaker.Many popular Word Processors use this format as a placeholder.Some examples are Pages or Microsoft Word.'
+    message: '    Lorem ipsu variation of the ordinary lorem ipsum text has been used in typesetting since the 1960s or earlier, when it was popularized by advertisements for Letraset transfer sheets.It was introduced to the Information Age in the mid - 1980s by Aldus Corporation, which employed it in graphics and word - processing templates for its desktop publishing program PageMaker.Many popular Word Processors use this format as a placeholder.Some examples are Pages or Microsoft Word.'
+  },
+  computed: {
+    reversedMessage: function () {
+      return this.message.split('').reverse().join('')
+    }
+  },
+  methods: {
+    reversedMessageMethod: function () {
+      return this.message.split('').reverse().join('')
+    }
   }
-  // filters: {
-  //   numberFormat: function (value) {
-  //     return value.toLocaleString()
-  //   }
-  // }
 
 })
