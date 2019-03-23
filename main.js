da2b841166609ae8dd08d8a27121584861278e2e
Logy0704 <example@example.com>
2019-03-23 16:29:12 +0900

    filter args

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-Vue.filter('toUSD', function (jpy) {
-  return jpy / 100
-})
 
-Vue.filter('numberFormat', function (value) {
-  return value.toLocaleString()
+Vue.filter('readMore', function (text, length, suffix) {
+  return text.substring(0, length) + suffix
 })
 var app = new Vue({
   el: '#app',
   data: {
-    jpyPrice: 29800000000000
+    text: '    Lorem ipsu variation of the ordinary lorem ipsum text has been used in typesetting since the 1960s or earlier, when it was popularized by advertisements for Letraset transfer sheets.It was introduced to the Information Age in the mid - 1980s by Aldus Corporation, which employed it in graphics and word - processing templates for its desktop publishing program PageMaker.Many popular Word Processors use this format as a placeholder.Some examples are Pages or Microsoft Word.'
   }
   // filters: {
   //   numberFormat: function (value) {
