0bcd8e489282696d132b9b2143faa99ebce4a5c4
Logy0704 <example@example.com>
2019-03-26 00:25:01 +0900

    event

--- main.js
+++ main.js
@@ -2,12 +2,13 @@
 var app = new Vue({
   el: '#app',
   data: {
-    counter: 0
+    counter: 0,
+    message: ''
   },
   methods: {
-    clickHandler: function (event) {
-      this.counter++
-      console.log(event)
+    clickHandler: function ($event, message) {
+      this.message = message
+      console.log($event)
     }
   }
 })
