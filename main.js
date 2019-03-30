de657822b6bcf9876cf30b0422af3096fd012522
Logy0704 <example@example.com>
2019-03-30 22:10:25 +0900

    transition

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-var helloComponent = {
-  template: '<p>Hello</p>'
-}
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++"> {{ count }}</button>'
+})
 var app = new Vue({
   el: '#app',
-  components: {
-    'hello-component': helloComponent
+  data: {
+    show: false
   }
 })
